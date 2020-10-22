1944c42b6d53cf4cfe03032bfc5754f4bf17fc64
Keating Reid <keating.reid@pm.me>
2020-10-22 14:10:48 -0400

    Fixing parse error related to final append of the operator stack

--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,194 @@
-#![feature(try_trait)]
-#![allow(unused_variables)]
-#![allow(unused_macros)]
-#![allow(dead_code)]
-#![allow(unused_imports)]
+use rustyline::{Config, EditMode, Editor, Helper};
+use std::{
+    collections::{HashMap, VecDeque},
+    env::args,
+    error::Error,
+    io,
+    process::exit,
+};
 
-use rustyline::{error::ReadlineError, Editor, EditMode, Helper, Config};
-use std::{collections::{HashSet, HashMap}, io, io::Read, option::NoneError};
-mod types;
-use crate::types::*;
-
-
-const PROMPT: &'static str = ">>> ";
-
-macro_rules! mkerr {
-    ($msg:tt) => { io::Error::new(io::ErrorKind::InvalidInput, $msg) };
-    ($kind:tt, $msg:tt) => { io::Error::new(io::ErrorKind::$kind, $msg) };
+macro_rules! readline {
+    ($rl:ident) => {
+        $rl.readline(">>> ")
+    };
 }
+macro_rules! mkerr {
+    ($msg:tt) => {
+        io::Error::new(io::ErrorKind::InvalidInput, $msg)
+    };
+    ($kind:tt, $msg:tt) => {
+        io::Error::new(io::ErrorKind::$kind, $msg)
+    };
+}
+
+const HELP_MSG: &'static str = "SYNTAX:
+-------
+negation:\t!
+and:\t\t&
+or:\t\t|
+superset:\t>";
 
 fn main() {
-    let mut rl = rustyline::Editor::<()>::new();
+    if args().len() != 1 {
+        eprintln!("{}", HELP_MSG);
+        exit(1);
+    }
+    let mut rl = Editor::<()>::with_config(
+        Config::builder()
+            .auto_add_history(true)
+            .tab_stop(4)
+            .edit_mode(EditMode::Vi)
+            .build(),
+    );
+    let vars = def_vars(&mut rl).unwrap();
+    loop {
+        println!("Enter your expression:");
+        let line = match readline!(rl) {
+            Ok(l) => l,
+            Err(_) => {
+                println!("Invalid expression");
+                continue;
+            }
+        };
+        let mut expr = match shunting_yard(&*line) {
+            Ok(v) => v,
+            Err(e) => {
+                eprintln!("{}", e);
+                continue;
+            }
+        };
+        match eval_postfix(&mut expr, &vars) {
+            Ok(v) => println!("Result: {}", v),
+            Err(_) => println!("Failed to evaluate expression"),
+        }
+    }
 }
 
-
 fn def_vars<H: Helper>(rl: &mut Editor<H>) -> io::Result<HashMap<char, bool>> {
-    // let mut vars: Vec<Prop> = Vec::with_capacity(2);
-    const HELP_MSG: &'static str = "Define your variables (syntax: p = true):";
-    println!("{}", HELP_MSG);
-    let mut vars: HashMap<char, bool> = HashMap::new();
-    while let Ok(mut line) = rl.readline(PROMPT) {
-        line.retain(|c| !c.is_whitespace());
-        if line.is_empty() { break; }
-        let tokens: Vec<&str> = line.split("=").collect();
-        let (var, val) = (tokens[0], tokens[1].parse());
-        let key = match var.len() {
-            0 => { eprintln!("No variable name received"); continue }
-            1 => var.chars().nth(0).unwrap(),
-            _ => { eprintln!("Variable names must a single letter."); continue}
+    macro_rules! print_help {
+        () => {
+            println!("Define your variables (syntax: p = true):")
         };
-        match val {
-            Ok(v) => { vars.insert(key,  v); }
-            _ => eprintln!("{}", HELP_MSG),
+        ($msg:literal) => {
+            println!(
+                concat!($msg, "\n{}"),
+                "Define your variables (syntax: p = true):"
+            )
+        };
+    }
+    let mut vars: HashMap<char, bool> = HashMap::new();
+    print_help!();
+    while let Ok(mut line) = readline!(rl) {
+        line.retain(|c| !c.is_whitespace());
+        if line.is_empty() {
+            break;
+        }
+        let tokens: Vec<&str> = line.split("=").collect();
+        let (var, val) = if tokens.len() == 2 {
+            (tokens[0], tokens[1])
+        } else {
+            print_help!("Invalid syntax");
+            continue;
+        };
+        let key = match var.len() {
+            0 => {
+                print_help!("No variable name received");
+                continue;
+            }
+            1 => var.chars().nth(0).unwrap(),
+            _ => {
+                print_help!("Variable names must a single letter");
+                continue;
+            }
+        };
+        match val.parse() {
+            Ok(v) => { vars.insert(key, v); }
+            _ => print_help!(),
         }
     }
     if vars.len() > 0 { Ok(vars) } else { Err(mkerr!("No variables received")) }
 }
 
-fn shunting_yard(line: &str) -> io::Result<Vec<char>> {
-    let mut operator_stack: Vec<char> = Vec::new();
-    let mut output_queue: Vec<char> = Vec::with_capacity(line.len());
+fn shunting_yard(line: &str) -> io::Result<VecDeque<char>> {
+    let mut operator_stack: VecDeque<char> = VecDeque::new();
+    let mut output_queue: VecDeque<char> = VecDeque::with_capacity(line.len());
     for c in line.chars().filter(|c| !c.is_whitespace()) {
         match c {
-            '&' | '|' => {
-                while !(operator_stack.is_empty() || *operator_stack.first().unwrap() == '(') {
-                    output_queue.push(operator_stack.pop().unwrap())
+            '&' | '|' | '>' => {
+                while match operator_stack.front() { Some('(') | None => false, _ => true } {
+                    output_queue.push_back(operator_stack.pop_back().unwrap())
                 }
-                operator_stack.push(c)
+                operator_stack.push_front(c)
             }
-            '!' | '(' => operator_stack.push(c),
-            ')' => match operator_stack.first() {
+            '!' | '(' => operator_stack.push_front(c),
+            ')' => match operator_stack.front() {
                 Some('(') => (),
-                Some(_) => operator_stack.push(c),
-                None => return Err(mkerr!("Unterminated parenthesis"))
+                Some(_) => operator_stack.push_front(c),
+                None => return Err(mkerr!("Unterminated parenthesis")),
             },
-            'a'..='z' | 'A'..='Z' => output_queue.push(c),
-            _ => return Err(mkerr!("Encountered unexpected symbol"))
+            'a'..='z' | 'A'..='Z' => output_queue.push_back(c),
+            other => {
+                let msg = format!("Encountered unexpected symbol {}", other);
+                return Err(mkerr!(msg));
+            }
         }
     }
-    output_queue.append(&mut operator_stack);
-    output_queue.retain(|c| *c != '(' && *c != ')');
+    while let Some(c) = operator_stack.pop_front() {
+        output_queue.push_back(c)
+    }
+    output_queue.retain(|c| !['(', ')'].contains(c));
     Ok(output_queue)
 }
 
-fn eval_postfix(expr: &mut Vec<char>, vars: &HashMap<char, bool>) -> Result<bool, NoneError> {
-    let mut stack: Vec<bool> = Vec::with_capacity(expr.len());
-    while let Some(c) = expr.pop() {
+fn eval_postfix(expr: &mut VecDeque<char>, vars: &HashMap<char, bool>) -> Result<bool, Box<dyn Error>> {
+    macro_rules! pop {
+        ($s:ident, $char:ident) => {
+            $s.pop().ok_or(format!(
+                "Syntax error: {} requires more arguments than provided",
+                $char
+            ))?
+        };
+    }
+    let mut stack: Vec<bool> = Vec::with_capacity(vars.len());
+    while let Some(c) = expr.pop_front() {
         match c {
             '!' => {
-                let a = stack.pop()?;
+                let a = pop!(stack, c);
                 stack.push(!a)
             }
             '|' => {
-                let (a, b) = (stack.pop()?, stack.pop()?);
+                let (a, b) = (pop!(stack, c), pop!(stack, c));
                 stack.push(a || b)
             }
             '&' => {
-                let (a, b) = (stack.pop()?, stack.pop()?);
+                let (a, b) = (pop!(stack, c), pop!(stack, c));
                 stack.push(a && b)
             }
             '>' => {
-                let result = match (stack.pop()?, stack.pop()?) {
+                let result = match (pop!(stack, c), pop!(stack, c)) {
                     (true, false) => false,
                     (_, _) => true,
                 };
                 stack.push(result);
             }
-            _ => stack.push(*vars.get(&c)?)
+            _ => stack.push(*vars.get(&c).ok_or(format!("Variable {} not recognized", c))?),
         }
     }
     if stack.len() == 1 {
         Ok(stack[0])
     } else {
-        Err(NoneError)
+        Err(Box::new(mkerr!( "Could not evaluate expression: Excess tokens received")))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::*;
-    use std::iter::FromIterator;
     #[test]
     fn test_shunting_yard() {
-        let v = shunting_yard("Q & P");
-        assert_eq!(v.unwrap(), vec!['Q', 'P', '&']);
-        let v1 = shunting_yard("!Q & P");
-        for t in v1.unwrap() {
-            print!("{} ", t);
-        }
-        println!();
+        assert_eq!(shunting_yard("Q & P").unwrap(), vec!['Q', 'P', '&']);
+        assert_eq!(shunting_yard("!Q & P").unwrap(), vec!['Q', '!', 'P', '&']);
+        assert_eq!(shunting_yard("Q | P").unwrap(), vec!['Q', 'P', '|']);
+        assert_eq!(shunting_yard("!Q & !P").unwrap(), vec!['Q', '!', 'P', '!', '&']);
     }
-
-    #[test]
-    fn test_eval_expr() {
-    }
-    // #[test]
-    // fn test_parse_expr() {
-    //     let e1 = "p and q";
-    //     let e1_set: HashSet<String> = HashSet::from_iter(["p", "q"].iter().map(|s| s.to_string()));
-    //     match parse_expr(e1.as_bytes(), e1_set) { _ => () };
-    // }
 }
